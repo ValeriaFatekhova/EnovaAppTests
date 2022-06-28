ca77ffc72dd4b65958b35a3fc7f584867d3cec0b
VFatekhova <kuminova_v@mail.ru>
2022-06-28 17:18:58 +0300

    Allure adding

--- Tests/test_sanity_enova_app.py
+++ Tests/test_sanity_enova_app.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 from Tests.test_base import BaseTest
 from Pages.LoginPage import LoginPage
@@ -42,33 +43,40 @@ class TestEnovaApp(BaseTest):
 
     """Show metrics test"""
 
+    @allure.description("Show metrics test")
     def test_show_metrics(self):
         self.settings = SettingsInApp(self.driver)
         self.customers_page = ChooseCustomerScreen(self.driver)
         self.enova_chat = EnovaChatPage(self.driver)
 
-        self.customers_page.customer_page_is_opened()
+        with allure.step("Switch on 'Show metrics' option in Settings"):
+            self.settings.show_metrix_switch_on()
+        with allure.step("Open chat for Enova customer"):
+            self.customers_page.open_chatmode_for_customer("Enova")
+        with allure.step("Make request in chat"):
+            self.enova_chat.send_question_in_chat_not_dialog(TestData.AUDIO_FOR_SINGLE_INTENTS[0][0])
 
-        self.settings.show_metrix_switch_on()
-        self.customers_page.open_chatmode_for_customer("Enova")
-        self.enova_chat.send_question_in_chat_not_dialog(TestData.AUDIO_FOR_SINGLE_INTENTS[0][0])
-
-        assert self.enova_chat.is_metrics_in_chat(), "Metrics is not present in chat after"
-        assert self.enova_chat.is_data_in_metrix(), "Metrics are empty, no data is in metrics"
+        with allure.step("Check that metrics are displayed in chat under system answer"):
+            assert self.enova_chat.is_metrics_in_chat(), "Metrics is not present in chat after"
+        with allure.step("Check that metrics contained not empty data"):
+            assert self.enova_chat.is_data_in_metrix(), "Metrics are empty, no data is in metrics"
 
     """Unregistering device test"""
 
+    @allure.description("Unregistering device test")
     @pytest.mark.parametrize("login_data", [TestData.LOGIN_DATA])
     def test_logout(self, login_data):
         self.login_page = LoginPage(self.driver)
         self.settings = SettingsInApp(self.driver)
         self.customers_page = ChooseCustomerScreen(self.driver)
 
-        self.customers_page.customer_page_is_opened()
+        with allure.step("Open Settings -> Device page and click 'Unregister' button"):
+            self.settings.unregister_device()
 
-        self.settings.unregister_device()
+        with allure.step("Check that login page is opened"):
+            assert self.login_page.is_login_page()
 
-        assert self.login_page.is_login_page()
-
-        self.login_page.click_send_button()
+        with allure.step("Check that 'Email' field is empty and after click 'Submit' button "
+                         "messsage about invalid email is displayed"):
+            self.login_page.click_send_button()
         assert self.login_page.is_warning_red_text()
